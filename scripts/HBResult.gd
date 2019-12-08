a6889d1b0a22d344d62b26a05f102310bbad9817
Álex Román Núñez <eirexe123@gmail.com>
2019-12-08 13:05:47 +0100

    Add result ratings to HBResult

--- scripts/HBResult.gd
+++ scripts/HBResult.gd
@@ -21,3 +21,30 @@ func _init():
 
 func get_serialized_type():
 	return "Result"
+
+enum RESULT_RATING {
+	FAIL,
+	CHEAP,
+	STANDARD,
+	GREAT,
+	EXCELLENT,
+	PERFECT
+}
+
+func get_result_rating():
+	var failure = note_ratings[HBJudge.JUDGE_RATINGS.SAD]
+	failure += note_ratings[HBJudge.JUDGE_RATINGS.SAFE]
+	failure += note_ratings[HBJudge.JUDGE_RATINGS.WORST]
+	if failure <= 0:
+		return RESULT_RATING.PERFECT
+		
+	var pass_results = note_ratings[HBJudge.JUDGE_RATINGS.FINE] + note_ratings[HBJudge.JUDGE_RATINGS.COOL]
+	pass_results = float(pass_results / float(total_notes))
+	if pass_results >= 0.97:
+		return RESULT_RATING.EXCELLENT
+	elif pass_results >= 0.94:
+		return RESULT_RATING.GREAT
+	elif pass_results >= 0.85:
+		return RESULT_RATING.STANDARD
+	else:
+		return RESULT_RATING.CHEAP
