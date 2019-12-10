6c56746f45e1667856cb26ab9a13cdc7506ffd91
Álex Román Núñez <eirexe123@gmail.com>
2019-12-10 21:28:52 +0100

    Add button to open the songs directory

--- menus/tools_menu/ToolsMenu.gd
+++ menus/tools_menu/ToolsMenu.gd
@@ -4,6 +4,7 @@ extends HBMenu
 func _ready():
 	$ToolsList/EditorButton.connect("pressed", self, "_on_EditorButton_pressed")
 	$ToolsList/SongMetaEditorButton.connect("pressed", self, "_on_SongMetaEditorButton_pressed")
+	$ToolsList/OpenSongsDirectoryButton.connect("pressed", self, "_on_OpenSongsDirectoryButton_pressed")
 func _on_menu_enter(force_hard_transition=false, args = {}):
 	._on_menu_enter(force_hard_transition, args)
 	$ToolsList.grab_focus()
@@ -18,3 +19,6 @@ func _unhandled_input(event):
 
 func _on_SongMetaEditorButton_pressed():
 	get_tree().change_scene_to(preload("res://tools/song_meta_editor/SongMetaEditor.tscn"))
+
+func _on_OpenSongsDirectoryButton_pressed():
+	OS.shell_open(ProjectSettings.globalize_path("user://songs"))
