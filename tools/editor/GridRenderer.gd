d9bb9f056842450fc0d2e0dd7cbd119471070ed6
Álex Román Núñez <eirexe123@gmail.com>
2019-12-23 09:10:16 +0100

    Change grid color

--- tools/editor/GridRenderer.gd
+++ tools/editor/GridRenderer.gd
@@ -11,12 +11,12 @@ func _draw():
 	for i in range(horizontal):
 		var first_point = Vector2(origin.x, size.y * (i/float(horizontal)))
 		var second_point = first_point + Vector2(size.x, 0)
-		draw_line(first_point, second_point, Color.yellow)
+		draw_line(first_point, second_point, Color(0.5, 0.5, 0.5))
 		
 	for i in range(vertical):
 		var first_point = Vector2(origin.x + size.x * i/float(vertical), 0)
 		var second_point = first_point + Vector2(0, size.y)
-		draw_line(first_point, second_point, Color.yellow)
+		draw_line(first_point, second_point, Color(0.5, 0.5, 0.5))
 
 func set_horizontal(value):
 	horizontal = value
