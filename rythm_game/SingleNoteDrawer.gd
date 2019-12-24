370c9efd875932e7f673450eae10bb10bc793e15
Álex Román Núñez <eirexe123@gmail.com>
2019-12-24 17:06:06 +0100

    trail styling improvements

--- rythm_game/SingleNoteDrawer.gd
+++ rythm_game/SingleNoteDrawer.gd
@@ -4,7 +4,7 @@ onready var target_graphic = get_node("NoteTarget")
 onready var note_graphic = get_node("Note")
 var pickable = true setget set_pickable
 export(float) var target_scale_modifier = 1.0
-const TRAIL_RESOLUTION = 32
+const TRAIL_RESOLUTION = 48
 
 var connected_note_judgements = {}
 
@@ -67,8 +67,8 @@ func set_trail_color():
 	var color2 = IconPackLoader.get_color(HBUtils.find_key(HBNoteData.NOTE_TYPE, note_data.note_type))
 	color1.a = 0.1
 	color2.a = 0.5
-	gradient.set_color(0, color1)
-	gradient.set_offset(0, 0.75)
+	gradient.set_color(0, color1.inverted())
+	gradient.set_offset(0, 0.25)
 	gradient.set_color(1, color2)
 	$Line2D.gradient = gradient
 	$Line2D2.gradient = gradient
@@ -85,8 +85,9 @@ func draw_trail(time: float):
 		var t_trail_time = trail_time * (i / float(TRAIL_RESOLUTION))
 		var t = ((time_out_distance - trail_time) + t_trail_time) / get_time_out()
 		var oscillation_amplitude = game.remap_coords(Vector2(1.0, 1)).x * note_data.oscillation_amplitude
-		var point1 = game.remap_coords(HBUtils.calculate_note_sine(t, note_data.position, note_data.entry_angle, note_data.oscillation_frequency, note_data.oscillation_amplitude, note_data.distance))
-		var point2 = game.remap_coords(HBUtils.calculate_note_sine(t, note_data.position, note_data.entry_angle, note_data.oscillation_frequency, note_data.oscillation_amplitude * 0.75, note_data.distance))
+		
+		var point1 = game.remap_coords(HBUtils.calculate_note_sine(t, note_data.position, note_data.entry_angle - deg2rad(15), note_data.oscillation_frequency, note_data.oscillation_amplitude, note_data.distance))
+		var point2 = game.remap_coords(HBUtils.calculate_note_sine(t, note_data.position, note_data.entry_angle + deg2rad(15), note_data.oscillation_frequency, note_data.oscillation_amplitude * 0.8, note_data.distance))
 		points2.append(point2)
 		points.append(point1)
 	$Line2D2.width = 6 * game.get_note_scale()
