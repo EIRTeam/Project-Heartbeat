56d4628c41e4d38ab2cdb0ded663e4b0e789a972
Álex Román Núñez <eirexe123@gmail.com>
2019-12-24 19:30:54 +0100

    Make HBVersion return GLES version too.

--- scripts/HBVersion.gd
+++ scripts/HBVersion.gd
@@ -3,11 +3,14 @@ extends Node
 class_name HBVersion
 
 static func get_version_string():
-	var result = "Project Heartbeat - %s (build %s, %s %s)"
+	var result = "Project Heartbeat (%s, %s) - %s (build %s, %s %s)"
 	var version = ProjectSettings.get("application/config/version")
-	var sha = ProjectSettings.get("application/config/build_commit")
+	var sha = ProjectSettings.get("application/config/build_commit").substr(0, 7)
 	var datetime = OS.get_datetime_from_unix_time(ProjectSettings.get("application/config/build_time")) 
 	var date = "%02d/%02d/%02d" % [datetime.day, datetime.month, datetime.year]
 	var time = " %02d:%02d:%d" % [datetime.hour, datetime.minute, datetime.second]
-	result = result % [version, sha, date, time]
+	var video_driver = OS.get_video_driver_name(OS.get_current_video_driver())
+	
+	
+	result = result % [video_driver, OS.get_name(), version, sha, date, time]
 	return result
