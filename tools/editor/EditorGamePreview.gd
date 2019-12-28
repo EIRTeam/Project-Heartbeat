6ae0710e584a85491a29f50a893f53d3376a5ee0
Álex Román Núñez <eirexe123@gmail.com>
2019-12-28 13:05:11 +0100

    Make safe area be the canonical size

--- tools/editor/EditorGamePreview.gd
+++ tools/editor/EditorGamePreview.gd
@@ -1,23 +1,26 @@
 extends Control
 
 onready var widget_area = get_node("WidgetArea")
+onready var game = get_node("RythmGame")
 const SAFE_AREA_FACTOR = 0.05
+const SAFE_AREA_SIZE = Vector2(192, 108)
+
 func _ready():
-	$RythmGame.size = rect_size
-	$RythmGame.editing = true
-	$RythmGame.input_lag_compensation = 0.0
+	game.size = rect_size
+	game.editing = true
+	game.input_lag_compensation = 0.0
 	connect("resized", self, "_on_resized")
 	
 func _on_resized():
-	$RythmGame.size = rect_size
+	game.size = rect_size
 	update()
 	
 func _process(delta):
-	$Label.text = HBUtils.format_time($RythmGame.time * 1000.0)
+	$Label.text = HBUtils.format_time(game.time * 1000.0)
 
 func _draw():
-	var origin = $RythmGame.remap_coords(Vector2())
-	var size = $RythmGame.get_playing_field_size()
+	var origin = game.remap_coords(Vector2())
+	var size = game.get_playing_field_size()
 	_draw_game_area(origin, size)
 	_draw_safe_area(origin, size)
 
@@ -25,5 +28,6 @@ func _draw_game_area(origin, size):
 	draw_rect(Rect2(origin, size), Color(1.0, 1.0, 1.0), false, 1.0, true)
 
 func _draw_safe_area(origin, size):
-	print(origin)
-	draw_rect(Rect2(origin + size*SAFE_AREA_FACTOR, size - size * SAFE_AREA_FACTOR * 2), Color(1.0, 0.0, 0.0), false, 1.0, true)
+	var GAME_AREA_START = game.remap_coords(Vector2.ZERO)
+	var safe_area_rect = Rect2(game.remap_coords(SAFE_AREA_SIZE), game.remap_coords(game.BASE_SIZE) + GAME_AREA_START - game.remap_coords(SAFE_AREA_SIZE) * 2)
+	draw_rect(safe_area_rect, Color(1.0, 0.0, 0.0), false, 1.0, true)
