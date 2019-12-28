b7c8362940825820de5d8c7d1dccef66774fa277
Álex Román Núñez <eirexe123@gmail.com>
2019-12-28 16:53:31 +0100

    Prevent finish_dragging fom firing multiple times in EditorWidget

--- tools/editor/widgets/EditorWidget.gd
+++ tools/editor/widgets/EditorWidget.gd
@@ -1,6 +1,10 @@
 extends Control
 var editor
 class_name HBEditorWidget
+signal property_changed(property_name, old_value, new_value)
 
 func _ready():
+	pass
+
+func _widget_area_input(event: InputEvent):
 	pass
