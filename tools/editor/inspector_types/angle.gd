a446db812a51bdddf8664040e68c70c22b23ded5
Álex Román Núñez <eirexe123@gmail.com>
2019-12-28 16:53:09 +0100

    Fix anglebox being dumb

--- tools/editor/inspector_types/angle.gd
+++ tools/editor/inspector_types/angle.gd
@@ -4,10 +4,10 @@ extends "res://tools/editor/inspector_types/EditorInspectorType.gd"
 
 func set_value(value):
 	if $Spinbox.is_connected("value_changed", self, "_on_Spinbox_value_changed"):
-		$Spinbox.disconnect("value_changed", self, "_on_Spinbox_value_changed")
+#		$Spinbox.disconnect("value_changed", self, "_on_Spinbox_value_changed")
 		$Spinbox.value = value
 		$AngleEdit.angle = value
-		$Spinbox.connect("value_changed", self, "_on_Spinbox_value_changed")
+#		$Spinbox.connect("value_changed", self, "_on_Spinbox_value_changed")
 
 func emit_value_changed_signal():
 	emit_signal("value_changed", $AngleEdit.angle)
