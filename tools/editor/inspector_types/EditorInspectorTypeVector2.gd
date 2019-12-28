d8fb947b812f3983713b42e47ad57c8a803b566b
Álex Román Núñez <eirexe123@gmail.com>
2019-12-28 16:53:55 +0100

    Make vector2 not be dumb

--- tools/editor/inspector_types/EditorInspectorTypeVector2.gd
+++ tools/editor/inspector_types/EditorInspectorTypeVector2.gd
@@ -1,6 +1,7 @@
 extends "res://tools/editor/inspector_types/EditorInspectorType.gd"
-
+var ignore_next_value_change = false
 func set_value(value):
+	ignore_next_value_change = true
 	$XSpinbox.value = value.x
 	$YSpinbox.value = value.y
 
@@ -9,7 +10,13 @@ func emit_value_changed_signal():
 	emit_signal("value_changed", Vector2($XSpinbox.value, $YSpinbox.value))
 
 func _on_XSpinbox_value_changed(value):
-	emit_value_changed_signal()
+	if not ignore_next_value_change:
+		emit_value_changed_signal()
+	else:
+		ignore_next_value_change = false
 
 func _on_YSpinbox_value_changed(value):
-	emit_value_changed_signal()
+	if not ignore_next_value_change:
+		emit_value_changed_signal()
+	else:
+		ignore_next_value_change = false
