46dbbafc8a0ac568649e768d9051044261b2ad12
Álex Román Núñez <eirexe123@gmail.com>
2020-01-05 15:58:52 +0100

    Improve tap detection code

--- autoloads/HBInput.gd
+++ autoloads/HBInput.gd
@@ -2,7 +2,22 @@ extends Node
 
 var pressed_inputs = {}
 var objects_processing = []
-const ACTIONS_TO_TRACK_FOR_TAPS = ["tap_right", "tap_left"]
+const ACTIONS_TO_TRACK_FOR_TAPS = ["tap_right_analog", "tap_left_analog"]
+
+const ANALOG_TO_DIGITAL_MAP = {
+	"tap_right_analog": "tap_right",
+	"tap_left_analog": "tap_left"
+}
+
+# device -> action -> axis -> pressed
+
+var device_map = {
+	
+}
+# device -> axis -> action
+var last_axis_values = {
+	
+}
 
 var debounce_polls = {}
 
@@ -19,27 +34,42 @@ func add_to_processing_unhandled_taps(object):
 func set_tap_as_handled():
 	current_tap_handled = true
 	
+func get_action_status_in_device_axis(action: String, device: int, axis: int):
+	if not device_map.has(device):
+		device_map[device] = {}
+	if not device_map[device].has(action):
+		device_map[device][action] = {}
+	if not device_map[device][action][axis]:
+		device_map[device][action][axis] = false
+	
 func _unhandled_input(event):
 	if event is InputEventJoypadMotion:
+#		for action in ACTIONS_TO_TRACK_FOR_TAPS:
+#			if event.is_action(action):
+#				print("ACTION!!!")
 		for action in debounce_polls:
 			debounce_polls[action] -= 1
 		for action in ACTIONS_TO_TRACK_FOR_TAPS:
 			if event.is_action(action):
-				
-				if event.get_action_strength(action) > 0.5:
+				if not last_axis_values.has(event.device):
+					last_axis_values[event.device] = {}
+				if not last_axis_values[event.device].has(action):
+					last_axis_values[event.device][action] = {}
+				if not last_axis_values[event.device][action].has(event.axis):
+					last_axis_values[event.device][action][event.axis] = 0.0
+				var last_value = last_axis_values[event.device][action][event.axis]
+				if last_value < 0.5 and event.get_action_strength(action) >= 0.5:
 					if pressed_inputs[action] == false:
-						if debounce_polls[action] <= 0:
-							pressed_inputs[action] = true
-							print("TAP")
-							debounce_polls[action] = 1
-							for i in range(objects_processing.size() -1, -1, -1):
-								var object = objects_processing[i]
-								if is_instance_valid(object) and not object.is_queued_for_deletion():
-									object._unhandled_tap(event)
-									if current_tap_handled:
-										current_tap_handled = false
-										break
-								else:
-									objects_processing.remove(i)
-				else:
-					pressed_inputs[action] = false
+						print("TAP")
+						var a = InputEventAction.new()
+						a.action = ANALOG_TO_DIGITAL_MAP[action]
+						a.pressed = true
+						Input.parse_input_event(a)
+
+				elif last_value >= 0.5 and event.get_action_strength(action) < 0.5:
+						print("UNTAP")
+						var a = InputEventAction.new()
+						a.action = ANALOG_TO_DIGITAL_MAP[action]
+						a.pressed = false
+						Input.parse_input_event(a)
+				last_axis_values[event.device][action][event.axis] = event.get_action_strength(action)
