c1fb0cd06296f0cfa2d872c66bca16979db01924
Álex Román Núñez <eirexe123@gmail.com>
2020-01-21 11:19:45 +0100

    Fix pause menu shenanigans

--- rythm_game/pause_menu/PauseMenu3D.gd
+++ rythm_game/pause_menu/PauseMenu3D.gd
@@ -3,3 +3,16 @@ extends Spatial
 signal resumed
 signal quit
 signal restart
+
+onready var resume_button = get_node("ViewportLeft/MarginContainer/VBoxContainer/HBListContainer/ResumeButton")
+onready var restart_button = get_node("ViewportLeft/MarginContainer/VBoxContainer/HBListContainer/RestartButton")
+onready var quit_button = get_node("ViewportLeft/MarginContainer/VBoxContainer/HBListContainer/QuitButton")
+onready var restart_popup = get_node("RestartPopup")
+onready var quit_popup = get_node("QuitPopup")
+onready var pause_menu_list = get_node("ViewportLeft/MarginContainer/VBoxContainer/HBListContainer")
+func _ready():
+	resume_button.connect("pressed", self, "emit_signal", ["resumed"])
+	restart_button.connect("pressed", restart_popup, "popup_centered_ratio", [0.35])
+	quit_button.connect("pressed", quit_popup, "popup_centered_ratio", [0.35])
+	quit_popup.connect("cancel", pause_menu_list, "grab_focus")
+	restart_popup.connect("cancel", pause_menu_list, "grab_focus")
