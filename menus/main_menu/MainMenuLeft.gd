2dcecb74e80771b60e301d760b823f519e461e06
Álex Román Núñez <eirexe123@gmail.com>
2020-02-26 15:19:52 +0100

    Hide MP when MP is not available

--- menus/main_menu/MainMenuLeft.gd
+++ menus/main_menu/MainMenuLeft.gd
@@ -4,7 +4,8 @@ onready var start_menu = get_node("StartMenu")
 
 func _ready():
 	start_menu.connect("navigate_to_menu", self, "change_to_menu")
-	
+	if not PlatformService.service_provider.implements_lobby_list:
+		get_tree().call_group("mponly", "queue_free")
 func _on_menu_enter(force_hard_transition=false, args = {}):
 	._on_menu_enter(force_hard_transition, args)
 	start_menu.grab_focus()
