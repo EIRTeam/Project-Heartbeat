625fee1f443b4f8d10f65a64b3bc3e6e23a47aa6
Álex Román Núñez <eirexe123@gmail.com>
2020-03-07 22:38:55 +0100

    Ensure score counter always takes at most 0.5 seconds to increase to the target score.

--- rythm_game/ScoreCounter.gd
+++ rythm_game/ScoreCounter.gd
@@ -1,17 +1,24 @@
 extends Label
 
-var score = 0
+var score = 0 setget set_score
 
 var _display_score = 0
 
-const INCREASE_SPEED = 2000 # points per second
+const SCORE_MAX_OUT_TIME = 0.5 # How much time it takes for the score to be applied
+
+var increase_speed = 2000 # points per second
+
+func set_score(val):
+	score = val
+	var diff = score - _display_score
+	increase_speed = diff / SCORE_MAX_OUT_TIME
 
 func _ready():
 	text = "%0*d" % [7, 0]
 
 func _process(delta):
 	if _display_score < score:
-		var speed = INCREASE_SPEED + (INCREASE_SPEED * _display_score / score)
+		var speed = increase_speed + (increase_speed * _display_score / score)
 		_display_score += clamp(int(delta * speed), 0, score)
 		text = "%0*d" % [7, _display_score]
 	else:
