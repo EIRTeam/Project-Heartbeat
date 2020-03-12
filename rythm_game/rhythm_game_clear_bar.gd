b12ed96ba4aac566fe56759138744702d453e0b7
Álex Román Núñez <eirexe123@gmail.com>
2020-03-12 20:25:13 +0100

    Fix rhythm game clear bar not drawing properly.

--- rythm_game/rhythm_game_clear_bar.gd
+++ rythm_game/rhythm_game_clear_bar.gd
@@ -1,8 +1,9 @@
 extends Panel
-var value = 0.0 setget set_value
+var value = 90.0 setget set_value
 var max_value = 100.0 setget set_max_value
 
 const PROGRESS_COLOR = Color("a877f0")
+
 export(Color) var CLEAR_COLOR = Color("000")
 
 func _ready():
@@ -24,18 +25,23 @@ func _draw():
 	var origin = Vector2(0,0)
 	var size = rect_size
 	size.x = size.x * (value / max_value)
-	var rect = Rect2(origin, size)
-	draw_rect(rect, PROGRESS_COLOR)
+	var rect_clear = Rect2(Vector2(rect_size.x * 0.85, 0), Vector2(rect_size.x * (1 - 0.85), rect_size.y))
+	draw_rect(rect_clear, CLEAR_COLOR)
+	var progress_rect = Rect2(origin, size)
+	draw_rect(progress_rect, PROGRESS_COLOR)
+	var past_completion_size = rect_size
+	past_completion_size.x = past_completion_size.x * ((value-(max_value*0.85)) / max_value)
+	if past_completion_size.x > 0:
+		var past_completion_progress_rect = Rect2(Vector2(rect_size.x * 0.85, 0), past_completion_size)
+		draw_rect(past_completion_progress_rect, PROGRESS_COLOR.darkened(0.5))
 	draw_rating_line(0.97)
 	draw_rating_line(0.94)
 	draw_rating_line(0.85)
-	var rect_clear = Rect2(Vector2(rect_size.x * 0.85, 0), Vector2(rect_size.x * (1 - 0.85), rect_size.y))
-	draw_rect(rect_clear, CLEAR_COLOR)
-	draw_line(origin+Vector2(rect_size.x * 0.85, -10), origin+Vector2(rect_size.x * 0.85, rect_size.y), PROGRESS_COLOR, 3)
+	draw_line(origin+Vector2(rect_size.x * 0.85, -12), origin+Vector2(rect_size.x * 0.85, rect_size.y), Color.red, 3)
 	$PercentageLabel.rect_position = Vector2(rect_size.x * 0.85, 0)
 	$PercentageLabel.rect_size = Vector2(rect_size.x * (1.0-0.85), rect_size.y)
-func draw_rating_line(val):
+func draw_rating_line(val, color=Color.white, height=10):
 	var val_r = round(value) / max_value
-	var origin = Vector2(rect_size.x * (val_r * val), -10)
+	var origin = Vector2(rect_size.x * (val_r * val), -height)
 	var end = Vector2(origin.x, rect_size.y)
-	draw_line(origin, end, Color.white, 3)
+	draw_line(origin, end, color, 3)
