473c1c12a434758eb78cbb845f6f5d06db64a195
Álex Román Núñez <eirexe123@gmail.com>
2020-03-20 14:49:00 +0100

    Fix steam avatars not showing up sometimes

--- autoloads/platform_service/multiplayer_services/SteamServiceMember.gd
+++ autoloads/platform_service/multiplayer_services/SteamServiceMember.gd
@@ -17,29 +17,28 @@ func _persona_state_change(steam_id, flags):
 		emit_signal("persona_state_change", flags)
 	
 func cache_steam_avatar():
-	if not steam_avatar_cached:
-		var img_handle = Steam.getMediumFriendAvatar(member_id)
-		var size = Steam.getImageSize(img_handle)
-		var buffer = Steam.getImageRGBA(img_handle).buffer
-		var avatar_image = Image.new()
-		var avatar_texture = ImageTexture.new()
-		avatar_image.create(size.width, size.height, false, Image.FORMAT_RGBAF)
-		
-		avatar_image.lock()
-		
-		for y in range(size.height):
-			for x in range(size.width):
-				var pixel = 4 * (x + y * size.height)
-				var r = float(buffer[pixel]) / 255.0
-				var g = float(buffer[pixel+1]) / 255.0
-				var b = float(buffer[pixel+2]) / 255.0
-				var a = float(buffer[pixel+3]) / 255.0
-				avatar_image.set_pixel(x, y, Color(r, g, b, a))
-		avatar_image.unlock()
-		avatar_image.save_png("user://avatar.png")
-		avatar_texture.create_from_image(avatar_image)
-		avatar = avatar_texture
-		steam_avatar_cached = true
+	var img_handle = Steam.getMediumFriendAvatar(member_id)
+	var size = Steam.getImageSize(img_handle)
+	var buffer = Steam.getImageRGBA(img_handle).buffer
+	var avatar_image = Image.new()
+	var avatar_texture = ImageTexture.new()
+	avatar_image.create(size.width, size.height, false, Image.FORMAT_RGBAF)
+	
+	avatar_image.lock()
+	
+	for y in range(size.height):
+		for x in range(size.width):
+			var pixel = 4 * (x + y * size.height)
+			var r = float(buffer[pixel]) / 255.0
+			var g = float(buffer[pixel+1]) / 255.0
+			var b = float(buffer[pixel+2]) / 255.0
+			var a = float(buffer[pixel+3]) / 255.0
+			avatar_image.set_pixel(x, y, Color(r, g, b, a))
+	avatar_image.unlock()
+	avatar_image.save_png("user://avatar.png")
+	avatar_texture.create_from_image(avatar_image)
+	avatar = avatar_texture
+	steam_avatar_cached = true
 
 func get_avatar():
 	if not steam_avatar_cached:
