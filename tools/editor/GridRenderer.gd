fc3808be577ccb8451283593caaafd741eeb92c7
Álex Román Núñez <eirexe123@gmail.com>
2020-06-09 02:16:18 +0200

    Add crosses marking multi note positions in the grid

--- tools/editor/GridRenderer.gd
+++ tools/editor/GridRenderer.gd
@@ -5,6 +5,8 @@ var vertical := 40 setget set_vertical
 
 onready var game = get_node("../RythmGame")
 
+const MULTI_TARGETS = [Vector2(240, 528), Vector2(720, 528), Vector2(1200, 528), Vector2(1680, 528)]
+
 func _draw():
 	var origin = game.remap_coords(Vector2())
 	var size = game.playing_field_size
@@ -17,7 +19,17 @@ func _draw():
 		var first_point = Vector2(origin.x + size.x * i/float(vertical), 0)
 		var second_point = first_point + Vector2(0, size.y)
 		draw_line(first_point, second_point, Color(0.5, 0.5, 0.5))
-
+	
+	for note_coord in MULTI_TARGETS:
+		draw_cross(game.remap_coords(note_coord))
+func draw_cross(cross_center: Vector2):
+	var height = (game.get_note_scale() * 100) / 2.0
+	var cross_vertical_start = cross_center - Vector2(0, height)
+	var cross_vertical_end = cross_center + Vector2(0, height)
+	var cross_horizontal_start = cross_center - Vector2(height, 0)
+	var cross_horizontal_end = cross_center + Vector2(height, 0)
+	draw_line(cross_vertical_start, cross_vertical_end, Color.white, 1.0)
+	draw_line(cross_horizontal_start, cross_horizontal_end, Color.white,1.0)
 func set_horizontal(value):
 	horizontal = value
 	update()
