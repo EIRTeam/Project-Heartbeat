d072fac81d8951c77931d73fb1164d03fb004ef2
Álex Román Núñez <eirexe123@gmail.com>
2020-07-02 17:09:35 +0200

    Bake shadows into sprite and remove note shadow shader.

--- graphics/build_atlases.py
+++ graphics/build_atlases.py
@@ -6,11 +6,27 @@ DIRECTORIES = [
 
 import subprocess
 import os
-
+import shutil
 base_cwd = os.getcwd()
 
 for directory in DIRECTORIES:
 	os.chdir(directory)
 	#atlasify -o sprite.png -ast -p 2 -m 1024,1024 --extrude 1 --trim --save ./
-	subprocess.call(["atlasify", "-o", "atlas.png", "-ast", "-p", "2", "--extrude", "1", "--trim", "-m", "-m 1024,1024", "--save", "./notes"])
+
+	# We will first bake the shadow for all sprites
+
+	# magick  convert heart.png \( +clone  -background black  -shadow 80x0+8+8 \) +swap -background none -layers merge +repage  shadow_hard.png
+
+	if os.path.isdir("notes_temp"):
+		shutil.rmtree("notes_temp")
+	os.mkdir("notes_temp")
+
+	for _file in os.listdir("notes"):
+		if _file.endswith(".png"):
+			if not "target" in _file and not "small" in _file:
+				subprocess.call(["magick", "convert", "notes/"+_file, "(", "+clone", "-background", "black", "-shadow", "80x0+10+10", ")", "+swap", "-background", "none", "-layers", "merge", "+repage", "-crop", "128x128+0+0", "notes_temp/" + _file])
+			else:
+				shutil.copy("notes/"+_file, "notes_temp/"+_file)
+	subprocess.call(["atlasify", "-ast", "-o", "atlas.png", "-p", "2", "--trim", "--extrude", "1", "-m 1024,1024", "--save", "./notes_temp"])
+	shutil.rmtree("notes_temp")
 	os.chdir(base_cwd)
