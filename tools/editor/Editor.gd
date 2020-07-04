b0b193c8a75118681b18de7c669da907ae7f4cae
Álex Román Núñez <eirexe123@gmail.com>
2020-07-05 00:18:37 +0200

    Fix issue that caused moving many notes to freeze the editor

--- tools/editor/Editor.gd
+++ tools/editor/Editor.gd
@@ -255,16 +255,18 @@ func _commit_selected_property_change(property_name: String):
 			if property_name in selected_item.data:
 				
 				undo_redo.add_do_property(selected_item.data, property_name, selected_item.data.get(property_name))
-				undo_redo.add_do_method(self, "_on_timing_points_changed")
 				undo_redo.add_do_method(selected_item._layer, "place_child", selected_item)
 				undo_redo.add_do_method(selected_item, "update_widget_data")
 				undo_redo.add_do_method(selected_item, "sync_value", property_name)
 
 				undo_redo.add_undo_property(selected_item.data, property_name, old_property_values[selected_item][property_name])
-				undo_redo.add_undo_method(self, "_on_timing_points_changed")
 				undo_redo.add_undo_method(selected_item._layer, "place_child", selected_item)
 				undo_redo.add_undo_method(selected_item, "update_widget_data")
 				undo_redo.add_undo_method(selected_item, "sync_value", property_name)
+	
+	undo_redo.add_do_method(self, "_on_timing_points_changed")
+	undo_redo.add_undo_method(self, "_on_timing_points_changed")
+	
 	undo_redo.commit_action()
 	inspector.sync_value(property_name)
 	release_owned_focus()
