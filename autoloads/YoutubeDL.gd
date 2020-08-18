9fd827c8212195cce1717e18b214748734b99a80
Álex Román Núñez <eirexe123@gmail.com>
2020-08-18 23:05:22 +0200

    Fix youtube-dl on windows

--- autoloads/YoutubeDL.gd
+++ autoloads/YoutubeDL.gd
@@ -166,7 +166,11 @@ func _download_video(userdata):
 		cache_meta.cache[userdata.video_id] = {}
 	var result = {"video_id": userdata.video_id}
 	# we have to ignroe the cache dir because youtube-dl is stupid
-	var shared_params = ["--ignore-config", "--no-cache-dir", "--ffmpeg-location", ProjectSettings.globalize_path(YOUTUBE_DL_DIR)]
+	var shared_params = ["--ignore-config", "--no-cache-dir"]
+	
+	if OS.get_name() == "X11":
+		shared_params += ["--ffmpeg-location", ProjectSettings.globalize_path(YOUTUBE_DL_DIR)]
+	
 	if download_audio:
 		var out = []
 		var temp_audio_path = get_audio_path(userdata.video_id, true, true).get_base_dir() + "/" + "%(id)s_temp.%(ext)s"
