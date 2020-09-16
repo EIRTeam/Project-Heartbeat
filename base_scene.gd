9e0c0e468486e6ea63b4bbb473fa7edf0ae2179c
Álex Román Núñez <eirexe123@gmail.com>
2020-09-16 10:57:31 +0200

    Readd hybrid loading but not crashy this time

--- base_scene.gd
+++ base_scene.gd
@@ -1,12 +1,17 @@
 extends Control
 
 onready var status_label = get_node("VBoxContainer/StatusLabel")
-const MAIN_MENU_PATH = "res://menus/MainMenu3D.tscn"
+const MAIN_MENU = preload("res://menus/MainMenu3D.tscn")
 
 const LOADINGU_SPEED = 0.5
 var loadingu_t = 0
+var timer = Timer.new()
 func _ready():
-	SongLoader.connect("all_songs_loaded", self, "_on_songs_finished_loading")
+	SongLoader.connect("all_songs_loaded", timer, "start")
+	set_status("Loadingu...")
+	add_child(timer)
+	timer.wait_time = 0.3
+	timer.connect("timeout", self, "_on_songs_finished_loading")
 	if OS.get_current_video_driver() == OS.VIDEO_DRIVER_GLES2:
 		VisualServer.viewport_set_hdr(get_viewport().get_viewport_rid(), false)
 func set_status(status: String):
@@ -16,24 +21,10 @@ func _process(delta):
 	loadingu_t += delta * LOADINGU_SPEED
 	
 	status_label.percent_visible = fmod(loadingu_t, 1.5)
-func _load_main_menu_thread(userdata):
-	call_deferred("_main_menu_loaded", userdata.thread, load(MAIN_MENU_PATH))
 
-func _input(event):
-	if event.is_action_pressed("free_friends"):
-		_on_songs_finished_loading()
-
-func _main_menu_loaded(thread: Thread, scene):
-	thread.wait_to_finish()
-	var new_scene = scene.instance()
+func _on_songs_finished_loading():
+	set_status("Loading main menu...")
+	var new_scene = MAIN_MENU.instance()
 	get_tree().current_scene.queue_free()
 	get_tree().root.add_child(new_scene)
 	get_tree().current_scene = new_scene
-
-func _on_songs_finished_loading():
-	yield(get_tree(), "idle_frame")
-	set_status("Loadingu...")
-	var thread = Thread.new()
-	var result = thread.start(self, "_load_main_menu_thread", { "thread": thread })
-	if result != OK:
-		Log.log(self, "Error starting thread for main menu loader: " + str(result), Log.LogLevel.ERROR)
