0397031acdb8075d5e693a887b1446274335f71d
Álex Román Núñez <eirexe123@gmail.com>
2020-09-16 11:01:46 +0200

    Add main menu 2d (not usable yet)

---
+++ menus/MainMenu2D.gd
@@ -0,0 +1,24 @@
+extends "res://menus/MainMenu.gd"
+
+const SLIDE_PARTICLES = preload("res://graphics/effects/SlideParticles.tscn")
+
+func _ready():
+	# We have to do this to force a shader compile beforehand because godot is
+	# stupid
+	yield(get_tree(), "idle_frame")
+	var particles = SLIDE_PARTICLES.instance()
+	add_child(particles)
+	particles.emitting = true
+	particles.position = Vector2(10000, 10000)
+	HBGame.rich_presence.update_activity({
+		"state": "On main menu"
+	})
+
+func menu_setup():
+	fullscreen_menu_container = get_node("FullscreenMenuContainer")
+	left_menu_container = get_node("MarginContainer/HBoxContainer/ContainerLeft")
+	right_menu_container = get_node("MarginContainer/HBoxContainer/ContainerRight")
+	first_background_texrect = get_node("CanvasLayer/Background1")
+	second_background_texrect = get_node("CanvasLayer/Background2")
+	background_transition_animation_player = get_node("CanvasLayer/AnimationPlayer")
+	change_to_menu(starting_menu, false, starting_menu_args)
