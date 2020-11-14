3274952f41c85dfa6d3cc532d280c19d958425d3
Álex Román Núñez <eirexe123@gmail.com>
2020-11-14 20:39:04 +0100

    Fix main menu music player graphics

--- menus/music_player/MainMenuMusicPlayer.gd
+++ menus/music_player/MainMenuMusicPlayer.gd
@@ -8,6 +8,11 @@ onready var main_container = get_node("MusicPlayer")
 
 var entry_tween = Tween.new()
 var exit_timer = Timer.new()
+
+var current_song: HBSong
+
+var background_assets_loader = HBBackgroundSongAssetsLoader.new()
+
 func _ready():
 	if UserSettings.user_settings.disable_menu_music:
 		hide()
@@ -17,12 +22,14 @@ func _ready():
 	main_container.modulate.a = 0.0
 	exit_timer.connect("timeout", self, "_on_exit_timer_timeout")
 	exit_timer.one_shot = true
+	background_assets_loader.connect("song_assets_loaded", self, "_on_assets_loaded")
 
 func format_time(secs: float) -> String:
 	return HBUtils.format_time(secs*1000.0, HBUtils.TimeFormat.FORMAT_MINUTES | HBUtils.TimeFormat.FORMAT_SECONDS)
 func set_song(song: HBSong, length: float, do_animation=true):
 	var playback_max_label = get_node("MusicPlayer/MarginContainer/VBoxContainer/HBoxContainer/VBoxContainer/VBoxContainer/PlaybackMaxLabel")
 
+	current_song = song
 
 	var song_title_label = get_node("MusicPlayer/MarginContainer/VBoxContainer/HBoxContainer/VBoxContainer/SongTitle")
 	current_song_length = length
@@ -35,6 +42,8 @@ func set_song(song: HBSong, length: float, do_animation=true):
 		entry_tween.start()
 	exit_timer.start()
 	
+	background_assets_loader.load_song_assets(song, ["preview"])
+	
 func _on_exit_timer_timeout():
 	entry_tween.stop_all()
 	entry_tween.interpolate_property(main_container, "modulate:a", 1.0, 0.0, 0.5, Tween.TRANS_LINEAR)
@@ -45,3 +54,10 @@ func set_time(time: float):
 	playback_current_time_label.text = format_time(time)
 	var playback_progress_bar = get_node("MusicPlayer/MarginContainer/VBoxContainer/HBoxContainer/VBoxContainer/VBoxContainer/VBoxContainer/ProgressBar")
 	playback_progress_bar.value = time / current_song_length
+
+func _on_assets_loaded(song, assets):
+	if song == current_song:
+		if "preview" in assets:
+			image_preview_texture_rect.texture = assets.preview
+		else:
+			image_preview_texture_rect.texture = DEFAULT_IMAGE_TEXTURE
