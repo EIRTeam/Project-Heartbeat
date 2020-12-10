ccd20ebc2a4848202332e7ea2bd186d3f72afae2
Álex Román Núñez <eirexe123@gmail.com>
2020-12-10 04:13:51 +0100

    Allow loading directly into the editor by using the --song-editor comand line argument.

--- base_scene.gd
+++ base_scene.gd
@@ -6,7 +6,13 @@ const MAIN_MENU = preload("res://menus/MainMenu3D.tscn")
 const LOADINGU_SPEED = 0.5
 var loadingu_t = 0
 var timer = Timer.new()
+
+var loading_editor = false
+
 func _ready():
+	if "--song-editor" in OS.get_cmdline_args():
+		loading_editor = true
+	
 	SongLoader.connect("all_songs_loaded", timer, "start")
 	if SongLoader.initial_load_done:
 		call_deferred("_on_songs_finished_loading")
@@ -26,7 +32,13 @@ func _process(delta):
 
 func _on_songs_finished_loading():
 	set_status("Loading main menu...")
-	var new_scene = MAIN_MENU.instance()
+	set_status("Loading editor...")
+	var new_scene
+
+	if not loading_editor:
+		new_scene = MAIN_MENU.instance()
+	else:
+		new_scene = load("res://tools/editor/Editor.tscn").instance()
 	get_tree().current_scene.queue_free()
 	get_tree().root.add_child(new_scene)
 	get_tree().current_scene = new_scene
