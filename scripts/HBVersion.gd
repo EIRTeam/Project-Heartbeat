f1bab057bc0ec930ba13d6b5e3e09c89d8ef74c9
Álex Román Núñez <eirexe123@gmail.com>
2021-01-15 04:14:05 +0100

    Bump version

--- scripts/HBVersion.gd
+++ scripts/HBVersion.gd
@@ -5,7 +5,7 @@ class_name HBVersion
 
 const MAJOR = 0
 const MINOR = 8
-const PATCH = 4
+const PATCH = 5
 
 const status = "Early Access"
 const ver_name = "Project Heartbeat: A Song of Fire and Goth Girls"
