7503004ae2fc8ffa8e4f83d3bbc90b775a31b9c0
Álex Román Núñez <eirexe123@gmail.com>
2021-02-05 19:44:48 +0100

    New note hit effect

--- graphics/effects/NoteHitEffect.gd
+++ graphics/effects/NoteHitEffect.gd
@@ -1,13 +1,44 @@
 extends Node2D
 
-onready var animation_player = get_node("AnimationPlayer")
+onready var note_icon = get_node("NoteIcon")
+onready var note_loop = get_node("Loop")
+onready var note_flare = get_node("Flare")
+
+var tween = Tween.new()
 
 func _ready():
+	add_child(tween)
 	play_effect()
-	animation_player.connect("animation_finished", self, "_on_animation_finished")
 	
 func play_effect():
-	$AnimationPlayer.play("COOL")
+	note_icon.scale = Vector2.ZERO
+	note_icon.modulate.a = 0.0
+	note_loop.scale = Vector2.ZERO
+	note_loop.modulate.a = 0.0
+	note_flare.scale = Vector2.ZERO
+	note_flare.modulate.a = 0.0
+	tween.playback_speed = 1.5
+	tween.stop_all()
+	var note_icon_t = Color.white
+	note_icon_t.a = 0.75
+	tween.interpolate_property(note_icon, "scale", Vector2(0.15, 0.15), Vector2(0.25, 0.25), 0.20, Tween.TRANS_BACK, Tween.EASE_IN)
+	tween.interpolate_property(note_icon, "scale", Vector2(0.25, 0.25), Vector2(0.65, 0.65), 0.5, Tween.TRANS_QUAD, Tween.EASE_OUT, 0.20)
+	tween.interpolate_property(note_icon, "modulate:a", 0.75, 0.0, 0.7)
+	
+	tween.interpolate_property(note_loop, "scale", Vector2(0.15, 0.15), Vector2(0.75, 0.75), 0.15, Tween.TRANS_BOUNCE, Tween.EASE_IN)
+	tween.interpolate_property(note_loop, "scale", Vector2(0.75, 0.75), Vector2(2.0, 2.0), 0.60, Tween.TRANS_QUAD, Tween.EASE_OUT, 0.15)
+	tween.interpolate_property(note_loop, "modulate:a", 0.6, 0.0, 0.7)
+	
+	tween.interpolate_property(note_flare, "scale", Vector2(0.15, 0.15), Vector2(0.75, 0.75), 0.15, Tween.TRANS_BOUNCE, Tween.EASE_IN)
+	tween.interpolate_property(note_flare, "scale", Vector2(0.75, 0.75), Vector2(1.0, 1.0), 0.25, Tween.TRANS_QUAD, Tween.EASE_OUT, 0.15)
+	tween.interpolate_property(note_flare, "modulate:a", 0.4, 0.0, 0.4)
+	
+	tween.start()
+#	$AnimationPlayer.play("COOL")
 	
 func _on_animation_finished(anim_name: String):
 	queue_free()
+
+func _input(event):
+	if event.is_action_pressed("free_friends"):
+		play_effect()
