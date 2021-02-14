0333459e465de9c2ec333c9ca1359658b3a4a6fd
Álex Román Núñez <eirexe123@gmail.com>
2021-02-14 23:08:05 +0100

    Note hit effect blend mode changes

--- graphics/effects/NoteHitEffect.gd
+++ graphics/effects/NoteHitEffect.gd
@@ -22,17 +22,21 @@ func play_effect():
 	tween.stop_all()
 	var note_icon_t = Color.white
 	note_icon_t.a = 0.75
+	
+	var transparent_trans = Tween.TRANS_QUINT
+	var transparent_ease = Tween.EASE_IN
+	
 	tween.interpolate_property(note_icon, "scale", Vector2(0.15, 0.15), Vector2(0.25, 0.25), 0.20, Tween.TRANS_BACK, Tween.EASE_IN)
 	tween.interpolate_property(note_icon, "scale", Vector2(0.25, 0.25), Vector2(0.65, 0.65), 0.5, Tween.TRANS_QUAD, Tween.EASE_OUT, 0.20)
-	tween.interpolate_property(note_icon, "modulate:a", 0.75, 0.0, 0.7)
+	tween.interpolate_property(note_icon, "modulate:a", 0.6, 0.0, 0.7, transparent_trans, transparent_ease)
 	
 	tween.interpolate_property(note_loop, "scale", Vector2(0.15, 0.15), Vector2(0.75, 0.75), 0.15, Tween.TRANS_BOUNCE, Tween.EASE_IN)
 	tween.interpolate_property(note_loop, "scale", Vector2(0.75, 0.75), Vector2(2.0, 2.0), 0.60, Tween.TRANS_QUAD, Tween.EASE_OUT, 0.15)
-	tween.interpolate_property(note_loop, "modulate:a", 0.6, 0.0, 0.7)
+	tween.interpolate_property(note_loop, "modulate:a", 0.6, 0.0, 0.7, transparent_trans, transparent_ease)
 	
 	tween.interpolate_property(note_flare, "scale", Vector2(0.15, 0.15), Vector2(0.75, 0.75), 0.15, Tween.TRANS_BOUNCE, Tween.EASE_IN)
 	tween.interpolate_property(note_flare, "scale", Vector2(0.75, 0.75), Vector2(1.0, 1.0), 0.25, Tween.TRANS_QUAD, Tween.EASE_OUT, 0.15)
-	tween.interpolate_property(note_flare, "modulate:a", 0.4, 0.0, 0.4)
+	tween.interpolate_property(note_flare, "modulate:a", 0.4, 0.0, 0.4, transparent_trans, transparent_ease)
 	
 	tween.start()
 #	$AnimationPlayer.play("COOL")
