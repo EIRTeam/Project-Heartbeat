8702649c99ef0669a3cd38433f7aae55eb12d88b
Álex Román Núñez <eirexe123@gmail.com>
2021-04-06 23:10:22 +0200

    Make visualizer smoother

--- menus/visualizer/Visualizer.gd
+++ menus/visualizer/Visualizer.gd
@@ -1,8 +1,8 @@
 extends Panel
 var spectrum : AudioEffectSpectrumAnalyzerInstance
-var FREQ_MAX = 44100.0 / 3 setget set_freq_max
+var FREQ_MAX = 44100.0 / 2.5 setget set_freq_max
 const VU_COUNT = 64 # high VU_COUNTS break on windows
-var MIN_DB = 60 setget set_min_db
+var MIN_DB = 70 setget set_min_db
 var spectrum_image := Image.new()
 var spectrum_image_texture := ImageTexture.new()
 export(StyleBox) var fallback_stylebox: StyleBox
@@ -16,9 +16,8 @@ func set_min_db(value):
 func _ready():
 	add_to_group("song_backgrounds")
 	spectrum = AudioServer.get_bus_effect_instance(1,0)
-	spectrum_image_texture.flags = 0 # disable filter to avoid bugs
 	spectrum_image.create(VU_COUNT,1,false,Image.FORMAT_R8)
-	spectrum_image_texture.create_from_image(spectrum_image)
+	spectrum_image_texture.create_from_image(spectrum_image, ImageTexture.FLAG_FILTER)
 	var mat := material as ShaderMaterial
 	mat.set_shader_param("audio", spectrum_image_texture)
 	mat.set_shader_param("FREQ_RANGE", VU_COUNT)
@@ -39,20 +38,18 @@ func _background_dim_changed(new_dim: float):
 		var box = fallback_stylebox as StyleBoxFlat
 		box.bg_color.a = 0.5 + (new_dim * 0.5)
 
-		
 func _physics_process(delta):
 	var prev_hz = 0
 	spectrum_image.lock()
-	for i in range(VU_COUNT):
+	for i in range(1, VU_COUNT+1):
 		var hz = i * (FREQ_MAX / VU_COUNT);
 		var f = spectrum.get_magnitude_for_frequency_range(prev_hz,hz, 1)
 		var energy = clamp((MIN_DB + linear2db(f.length()))/MIN_DB,0,1)
 		
-		spectrum_image.set_pixel(i, 0, Color(energy, 1.0, 1.0))
-		if (i == -1):
-			spectrum_image.set_pixel(i, 0, Color(1.0, 0.0, 0.0))
-			
+		spectrum_image.set_pixel(i-1, 0, Color(energy, 1.0, 1.0))
+		
 		prev_hz = hz
+	#spectrum_image.set_pixel(0, 0, Color(1.0, 0.0, 0.0))
 	spectrum_image.unlock()
 	spectrum_image_texture.set_data(spectrum_image)
 	var mat := material as ShaderMaterial
