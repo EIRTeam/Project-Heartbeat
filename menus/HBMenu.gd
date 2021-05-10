7aab8d96b993b2b7a012e87de749035f5bc3d769
Álex Román Núñez <eirexe123@gmail.com>
2021-05-10 03:38:31 +0200

    Bounci menu transition animation

--- menus/HBMenu.gd
+++ menus/HBMenu.gd
@@ -6,11 +6,9 @@ class_name HBMenu
 
 onready var tween = Tween.new()
 
-const TRANSITION_DURATION = 0.25
-
 export(bool) var transitions_enabled = true
 
-const TRANSITION_TYPE = Tween.TRANS_QUAD
+const TRANSITION_TYPE = Tween.TRANS_LINEAR
 
 func _ready():
 	add_child(tween)
@@ -31,11 +29,13 @@ func _on_menu_enter(force_hard_transition=false, args = {}):
 	
 	var starting_position = (rect_size - rect_size*starting_scale) /2
 	var target_position = Vector2(0.0, 0.0)
+	show()
+	rect_pivot_offset = rect_size / 2.0
 	
 	if transitions_enabled and not force_hard_transition:
-		tween.interpolate_property(self, "modulate", starting_color, target_color, TRANSITION_DURATION, TRANSITION_TYPE, Tween.EASE_IN_OUT)
-		tween.interpolate_property(self, "rect_scale", starting_scale, target_scale, TRANSITION_DURATION, TRANSITION_TYPE, Tween.EASE_IN_OUT)
-		tween.interpolate_property(self, "rect_position", starting_position, target_position, TRANSITION_DURATION, TRANSITION_TYPE, Tween.EASE_IN_OUT)
+		tween.interpolate_property(self, "modulate", starting_color, target_color, 0.15, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+		tween.interpolate_property(self, "rect_scale", starting_scale, target_scale, 0.25, Tween.TRANS_BACK, Tween.EASE_OUT)
+		
 		tween.start()
 	else:
 		hide()
@@ -51,11 +51,11 @@ func _on_menu_exit(force_hard_transition = false):
 	var starting_position = rect_position
 	var target_position = (rect_size - rect_size*target_scale)/2
 	
+	rect_pivot_offset = rect_size / 2.0
 	
 	if transitions_enabled and not force_hard_transition:
-		tween.interpolate_property(self, "modulate", starting_color, target_color, TRANSITION_DURATION, TRANSITION_TYPE, Tween.EASE_IN_OUT)
-		tween.interpolate_property(self, "rect_scale", starting_scale, target_scale, TRANSITION_DURATION, TRANSITION_TYPE, Tween.EASE_IN_OUT)
-		tween.interpolate_property(self, "rect_position", starting_position, target_position, TRANSITION_DURATION, TRANSITION_TYPE, Tween.EASE_IN_OUT)
+		tween.interpolate_property(self, "modulate", starting_color, target_color, 0.15, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+		tween.interpolate_property(self, "rect_scale", starting_scale, target_scale, 0.25, Tween.TRANS_BACK, Tween.EASE_IN)
 		tween.start()
 	else:
 		hide()
