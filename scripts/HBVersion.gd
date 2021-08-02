9b0ec406dc9f40238b1402380644bdb6278ab4af
Álex Román Núñez <eirexe123@gmail.com>
2021-08-02 08:41:49 +0200

    Bump version to 0.11.3

--- scripts/HBVersion.gd
+++ scripts/HBVersion.gd
@@ -5,7 +5,7 @@ class_name HBVersion
 
 const MAJOR = 0
 const MINOR = 11
-const PATCH = 2
+const PATCH = 3
 
 const status = "Early Access"
 const ver_name = "Project Heartbeat: Goth Girl Armageddon"
