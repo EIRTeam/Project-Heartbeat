58f36873a38a253d6f336acea097e28e425f0342
Álex Román Núñez <eirexe123@gmail.com>
2021-09-12 04:07:34 +0200

    Add pp display in main menu

--- menus/MainMenuUserInfo.gd
+++ menus/MainMenuUserInfo.gd
@@ -5,6 +5,9 @@ onready var avatar_texture_rect = get_node("MarginContainer/HBoxContainer/Textur
 onready var username_field = get_node("MarginContainer/HBoxContainer/VBoxContainer/HBoxContainer2/NameLabel")
 onready var level_field = get_node("MarginContainer/HBoxContainer/VBoxContainer/HBoxContainer2/LevelLabel")
 onready var exp_progress_bar = get_node("MarginContainer/HBoxContainer/VBoxContainer/HBoxContainer/ProgressBar")
+onready var rating_label = get_node("MarginContainer/HBoxContainer/VBoxContainer/HBoxContainer2/RatingLabel")
+onready var rating_label_vseparator = get_node("MarginContainer/HBoxContainer/VBoxContainer/HBoxContainer2/VSeparator2")
+onready var level_label_vseparator = get_node("MarginContainer/HBoxContainer/VBoxContainer/HBoxContainer2/VSeparator")
 
 func _ready():
 	avatar_texture_rect.texture = PlatformService.service_provider.get_avatar()
@@ -17,3 +20,10 @@ func load_from_user_data():
 	exp_progress_bar.max_value = max_exp
 	exp_progress_bar.value = HBBackend.user_info.experience
 	username_field.text = PlatformService.service_provider.friendly_username
+	rating_label.text = "%.0f pp" % HBBackend.user_info.rating
+
+	rating_label.visible = HBBackend.has_user_data
+	level_field.visible = HBBackend.has_user_data
+	rating_label_vseparator.visible = HBBackend.has_user_data
+	level_label_vseparator.visible = HBBackend.has_user_data
+	exp_progress_bar.visible = HBBackend.has_user_data
--- scripts/HBWebUserInfo.gd
+++ scripts/HBWebUserInfo.gd
@@ -6,10 +6,12 @@ var id: int = 0
 var steam_id: String = ""
 var is_admin: bool = false
 var level: int = 1
-var experience: int = 0 
+var experience: int = 0
+var rating: float = 0.0
 
 func _init():
-	serializable_fields += ["id", "steam_id", "is_admin", "level", "experience"]
+	serializable_fields += ["id", "steam_id", "is_admin", "level", "experience",
+		"rating" ]
 
 func get_serialized_type():
 	return "WebUserInfo"
