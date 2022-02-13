52bf6d1d56b28a72f10ddaf322243ce96f78238d
Álex Román Núñez <eirexe123@gmail.com>
2022-02-13 18:15:41 +0100

    Bump version to 0.13.9

--- scripts/HBVersion.gd
+++ scripts/HBVersion.gd
@@ -5,7 +5,7 @@ class_name HBVersion
 
 const MAJOR = 0
 const MINOR = 13
-const PATCH = 8
+const PATCH = 9
 
 const status = "Early Access"
 const ver_name = "Project Heartbeat: Goth Girl Supremacy"
