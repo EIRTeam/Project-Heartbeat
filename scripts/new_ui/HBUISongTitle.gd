20e38b6160f0c2f32c8c0658fcdebdf3f10e4701
Álex Román Núñez <eirexe123@gmail.com>
2022-05-25 13:45:47 +0200

    Add the ability to hide title elements in the skin editor

--- scripts/new_ui/HBUISongTitle.gd
+++ scripts/new_ui/HBUISongTitle.gd
@@ -11,6 +11,28 @@ onready var artist_label := HBUIDynamicLabel.new()
 var artist_font := HBUIFont.new() setget set_artist_font
 var title_font := HBUIFont.new() setget set_title_font
 var element_separation := 15 setget set_element_separation
+var hide_artist_label := false setget set_hide_artist_label
+var hide_circle_logo := false setget set_hide_circle_logo
+var hide_title_label := false setget set_hide_title_label
+
+func set_hide_artist_label(val):
+	hide_artist_label = val
+	if is_inside_tree():
+		artist_label.visible = !val
+
+func set_hide_title_label(val):
+	hide_title_label = val
+	if is_inside_tree():
+		title_label.visible = !val
+
+func set_hide_circle_logo(val):
+	hide_circle_logo = val
+	if is_inside_tree():
+		if circle_logo_texture_rect.texture:
+			if not hide_circle_logo:
+				circle_logo_margin_container.show()
+		if hide_circle_logo:
+			circle_logo_margin_container.hide()
 
 func set_element_separation(val):
 	element_separation = val
@@ -36,7 +58,8 @@ static func get_component_name() -> String:
 func get_hb_inspector_whitelist() -> Array:
 	var whitelist := .get_hb_inspector_whitelist()
 	whitelist.append_array([
-		"artist_font", "title_font", "element_separation"
+		"artist_font", "title_font", "element_separation", "hide_artist_label",
+		"hide_circle_logo", "hide_title_label"
 	])
 	return whitelist
 
@@ -69,6 +92,8 @@ func _ready():
 	set_element_separation(element_separation)
 	set_artist_font(artist_font)
 	set_title_font(title_font)
+	set_hide_artist_label(hide_artist_label)
+	set_hide_circle_logo(hide_artist_label)
 	connect("resized", self, "_on_resized")
 
 func _on_resized():
@@ -84,6 +109,7 @@ func set_song(song: HBSong, assets, variant := -1):
 	if not assets:
 		var circle_logo_path = song.get_song_circle_logo_image_res_path()
 		if circle_logo_path:
+			circle_logo_margin_container.show()
 			circle_logo_texture_rect.show()
 			var image = HBUtils.image_from_fs(circle_logo_path)
 			var it = ImageTexture.new()
@@ -98,8 +124,10 @@ func set_song(song: HBSong, assets, variant := -1):
 				circle_logo_margin_container.show()
 				circle_logo_texture_rect.texture = assets.circle_logo
 				_on_resized()
+	if hide_circle_logo:
+		circle_logo_margin_container.hide()
 	title_label.text = song.get_visible_title(variant)
-	artist_label.visible = !song.hide_artist_name
+	artist_label.visible = !song.hide_artist_name and not hide_artist_label
 	if song.artist_alias != "":
 		artist_label.text = song.artist_alias
 	else:
@@ -110,6 +138,9 @@ func _to_dict(resource_storage: HBInspectorResourceStorage) -> Dictionary:
 	out_dict["artist_font"] = serialize_font(artist_font, resource_storage)
 	out_dict["title_font"] = serialize_font(title_font, resource_storage)
 	out_dict["element_separation"] = element_separation
+	out_dict["hide_circle_logo"] = hide_circle_logo
+	out_dict["hide_artist_label"] = hide_artist_label
+	out_dict["hide_title_label"] = hide_title_label
 	return out_dict
 	
 func _from_dict(dict: Dictionary, cache: HBSkinResourcesCache):
@@ -117,3 +148,6 @@ func _from_dict(dict: Dictionary, cache: HBSkinResourcesCache):
 	deserialize_font(dict.get("artist_font", {}), artist_font, cache)
 	deserialize_font(dict.get("title_font", {}), title_font, cache)
 	element_separation = dict.get("element_separation", 15)
+	hide_circle_logo = dict.get("hide_circle_logo", false)
+	hide_artist_label = dict.get("hide_artist_label", false)
+	hide_title_label = dict.get("hide_title_label", false)
