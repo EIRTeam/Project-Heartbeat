997a04f8f3d64fb20000b1a193170522c0ade7e1
Lino Bigatti <linobigatti@protonmail.com>
2022-12-21 09:49:46 -0300

    Feature: Add interpolation to the contextual menu for MultiSpinbox.

--- tools/editor/HBEditorMultiSpinBox.gd
+++ tools/editor/HBEditorMultiSpinBox.gd
@@ -2,8 +2,16 @@ extends MultiSpinBox
 
 class_name HBEditorMultiSpinBox
 
+const INTERPOLATE_ID = 8
+
 func _ready():
 	connect("error_changed", self, "_on_error")
+	
+	var menu := get_line_edit().get_menu()
+	menu.connect("id_pressed", self, "_on_menu_id_pressed")
+	
+	menu.add_separator()
+	menu.add_item("Interpolate property", INTERPOLATE_ID, get_shortcut(KEY_F, ["ctrl"]))
 
 func _input(event):
 	if event is InputEventMouseButton and not get_global_rect().has_point(get_global_mouse_position()):
@@ -13,3 +21,30 @@ func _input(event):
 
 func _on_error(error_text):
 	hint_tooltip = error_text
+
+func get_shortcut(keycode: int, modifier: Array = []) -> int:
+	var i := InputEventKey.new()
+	
+	i.control = modifier.has("ctrl")
+	i.shift = modifier.has("shift")
+	i.alt = modifier.has("alt")
+	i.scancode = keycode
+	
+	return i.get_scancode_with_modifiers()
+
+func _on_menu_id_pressed(id: int):
+	var property = property_name if not inner_property_name else "%s.%s" % [property_name, inner_property_name]
+	
+	if id == INTERPOLATE_ID:
+		var t = ""
+		if property_name == "time":
+			t = "i * (1 / float(notes.size() - 1))"
+		else:
+			t = "float(note.time - notes[0].time) / float(notes[notes.size() - 1].time - notes[0].time)"
+		
+		execute("lerp(notes[0].%s, notes[notes.size() - 1].%s, %s)" % [property, property, t])
+		
+		# HACK: Clear the array warning, since this is not an user-provided expression
+		set_block_signals(true)
+		execute("note.%s" % property)
+		set_block_signals(false)
