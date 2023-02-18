e199aaa9394fcd066802439c7918ca3ebdd7c3d4
Lino Bigatti <linobigatti@protonmail.com>
2023-02-18 11:19:34 -0300

    Feature: Move FPS counter to the righthandside.

--- tools/editor/EditorBottomBar.gd
+++ tools/editor/EditorBottomBar.gd
@@ -4,4 +4,4 @@ func _ready():
 	$HBoxContainer/VersionLabel.text = HBVersion.get_version_string().split("-", true, 1)[1].rsplit("-", true, 1)[0]
 
 func _process(delta):
-	$FPSLabel.text = "%d FPS" % [Engine.get_frames_per_second()]
+	$HBoxContainer/FPSLabel.text = "%d FPS" % [Engine.get_frames_per_second()]
