28c4f5954b9b2a19878dee38d62be7789bda5dd5
Álex Román Núñez <eirexe123@gmail.com>
2023-03-14 22:01:18 +0100

    Add dime linda

--- menus/options_menu/OptionCreditsSection.gd
+++ menus/options_menu/OptionCreditsSection.gd
@@ -23,15 +23,16 @@ const PH_AUTHORS = {
 		"Emmilly Lima (sadnesswaifu)": ["Background artwork (Imademo, Blossoming Spell, Getaway)"],
 		"Albert Navarro (Reixart)": ["Background artwork (Going my way)"],
 		"Andrea Balaguer (Hikaru)": ["Background artwork (RedLine)"],
-		"David Revoy": ["Used background artwork (Hyperspeed out of Control, Music Play in the Floor)"],
+		"David Revoy": ["Used background artwork (Hyperspeed out of Control, Music Play in the Floor, Dime Linda)"],
 		"Lucas Corral": ["Background artwork (Reprisal)"]
 	},
 	"Charting": {
-		"Hunter Stevens (Yahoo)": ["Going my Way", "Through the Night", "Love Sacrifice", "Reprisal", "Confession"],
+		"Hunter Stevens (Yahoo)": ["Going my Way", "Through the Night", "Love Sacrifice", "Reprisal", "Confession", "Monochrome Stars"],
 		"Twoncity": ["Cloudy Sky 2019", "Blossoming Spell", "Connected", "Imademo"],
 		"Starbeat": ["Getaway"],
 		"Blizzin": ["Sands of Time", "RedLine", "Imademo", "Core creation"],
-		"Snail": ["ETERNAL"]
+		"Snail": ["ETERNAL"],
+		"BunBun": ["Dime Linda"]
 	},
 	"Music": {
 		"SuganoMusic": [
@@ -59,12 +60,32 @@ const PH_AUTHORS = {
 		],
 		"TORAV4": [
 			"Connected"
+		],
+		"Mixie": [
+			"Dime Linda"
+		]
+	},
+	"Charting software": {
+		"PPD Editor (by KHCMaster)": [
+			"Imademo 2012",
+			"Sands of Time",
+			"Getaway",
+			"Blossoming Spell",
+			"Cloudy Sky 2019",
+			"Music Play in the Floor",
+			"RedLine 2018",
+			"Hyperspeed out of Control",
+			"Connected",
+		],
+		"Comfy Studio (by samyuu)": [
+			"Dime Linda"
 		]
 	},
 	"Licenses" : [
 		"[u]\"cover book project\"[/u] by David Revoy, licensed under Creative Commons Attribution 4.0.",
 		"[u]A derivative of \"Episode 26 page 1\"[/u] by David Revoy (cropped), licensed under Creative Commons Attribution 4.0.",
 		"[u]A derivative of \"Episode 33 page 2\"[/u] by David Revoy (cropped), licensed under Creative Commons Attribution 4.0.",
+		"[u]\"Saffron steampunk clothes\"[/u] by David Revoy, licensed under Creative Commons Attribution 4.0.",
 		"[u]Open source Nintendo Switch toolchain[/u] by devkitPro"
 	],
 	"Others": {
@@ -152,6 +173,10 @@ func show_ph_credits(label: RichTextLabel):
 	label.bbcode_text += "\n"
 	make_title(label, "Charting")
 	make_credits_list(label, PH_AUTHORS.Charting)
+
+	label.bbcode_text += "\n"
+	make_title(label, "Charting Software")
+	make_credits_list(label, PH_AUTHORS["Charting software"])
 
 	label.bbcode_text += "\n"
 	make_title(label, "Licenses")
