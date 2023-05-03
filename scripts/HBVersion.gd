91fa383860f83e856decb22f8468e29d504fb0c0
Álex Román Núñez <eirexe123@gmail.com>
2023-05-03 10:05:03 +0200

    Bump version to 0.18

--- scripts/HBVersion.gd
+++ scripts/HBVersion.gd
@@ -4,11 +4,11 @@ extends Node
 class_name HBVersion
 
 const MAJOR = 0
-const MINOR = 17
-const PATCH = 2
+const MINOR = 18
+const PATCH = 0
 
 const status = "Early Access"
-const ver_name = "Project Heartbeat: Goth Girl Galaxy"
+const ver_name = "Project Heartbeat: Goth Girl Racing Department"
 
 static func get_version_string(with_line_breaks := false):
 	var result = "{ver_name} - {status} ({video_driver}, {audio_driver}, {os_name}) - {version} (build {commit}, {build_date} {build_time})"
