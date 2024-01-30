33f634629c5f9965c9c231b09dc82b1bac6205bb
Álex Román Núñez <eirexe123@gmail.com>
2024-01-30 02:33:40 +0100

    Move particles to CPU

--- graphics/effects/SlideParticles.gd
+++ graphics/effects/SlideParticles.gd
@@ -1,4 +1,5 @@
-extends GPUParticles2D
+@uid("uid://bx4ofak8m5q6b") # Generated automatically, do not modify.
+extends CPUParticles2D
 
 func _ready():
 	emitting = true
--- graphics/effects/SliderPieceParticle.gd
+++ graphics/effects/SliderPieceParticle.gd
@@ -1,15 +1,17 @@
+@uid("uid://ep2vlj4qg55x") # Generated automatically, do not modify.
 @tool
-extends GPUParticles2D
+extends CPUParticles2D
 
 class_name SliderPieceParticle
 
 func do_emit():
 	restart()
-	for i in [1, -1]:
-		var particle_pos := Vector2.RIGHT.rotated(deg_to_rad(25.0) * i)
-		particle_pos *= 75.0
-		var particle_trf := get_transform() * Transform2D(deg_to_rad(45.0), Vector2(0.25, 0.25), 0, particle_pos)
-		emit_particle(particle_trf, Vector2(1250.0, -i * 100.0), Color.WHITE, Color.WHITE, EMIT_FLAG_POSITION | EMIT_FLAG_ROTATION_SCALE | EMIT_FLAG_VELOCITY)
+	#for i in [1, -1]:
+		#var particle_pjos := Vector2.RIGHT.rotated(deg_to_rad(25.0) * i)
+		#particle_pos *= 75.0
+		#var particle_trf := Transform2D(deg_to_rad(45.0), Vector2(0.25, 0.25), 0, particle_pos)
+		#emit_particle(particle_trf, Vector2(1250.0, -i * 100.0), Color.WHITE, Color.WHITE, EMIT_FLAG_POSITION | EMIT_FLAG_ROTATION_SCALE | EMIT_FLAG_VELOCITY)
+	emitting = true
 	$GPUParticles2D.emitting = true
 func _ready():
 	if Engine.is_editor_hint():
