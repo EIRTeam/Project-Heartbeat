16cfc8ff7ff51ed8bc7ef2c0f937fe339aa15cfe
Álex Román Núñez <eirexe123@gmail.com>
2024-04-14 10:24:44 +0200

    Fix even more pregame tab clipipng issues

--- menus/pregame_screen/PregameStartTab.gd
+++ menus/pregame_screen/PregameStartTab.gd
@@ -1,10 +1,10 @@
 extends TabbedContainerTab
 
 @onready var button_container = get_node("Panel2/MarginContainer/VBoxContainer")
-@onready var modifier_button_container = get_node("Panel/HBoxContainer/VBoxContainer/HBoxContainer/Panel/MarginContainer/ScrollContainer/VBoxContainer")
+@onready var modifier_button_container = get_node("%ModifierButtonContainer")
 @onready var modifier_scroll_container = get_node("Panel/HBoxContainer/VBoxContainer/HBoxContainer/Panel/MarginContainer/ScrollContainer")
 @onready var button_panel = get_node("Panel2")
-@onready var leaderboard_legal_text = get_node("Panel/HBoxContainer/VBoxContainer/Panel2/HBoxContainer/Label")
+@onready var leaderboard_legal_text = get_node("%LeaderboardLegalLabel")
 @onready var stats_label = get_node("%StatsLabel")
 @onready var start_button = get_node("Panel2/MarginContainer/VBoxContainer/StartButton")
 @onready var start_practice_button = get_node("Panel2/MarginContainer/VBoxContainer/StartPractice")
