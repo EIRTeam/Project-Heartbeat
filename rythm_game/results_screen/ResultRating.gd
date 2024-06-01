9f7ad21caca1a1695c3d12e8e65722d170d57252
Álex Román Núñez <eirexe123@gmail.com>
2024-06-01 13:41:15 +0200

    Results screen improvements part 4

--- rythm_game/results_screen/ResultRating.gd
+++ rythm_game/results_screen/ResultRating.gd
@@ -5,10 +5,10 @@ class_name HBResultScreenRating
 
 @export var rating: HBJudge.JUDGE_RATINGS : set = set_rating
 var total_notes : set = set_total_notes
-var percentage : set = set_percentage
+var percentage: float : set = set_percentage
 
 @onready var total_label = get_node("MarginContainer/HBoxContainer/ValueLabel3/TotalLabel") 
-@onready var rating_label = get_node("MarginContainer/HBoxContainer/RatingLabel")
+@onready var rating_rect: TextureRect = get_node("%RatingRect")
 @onready var percentage_label = get_node("MarginContainer/HBoxContainer/ValueLabel3/PercentageLabel")
 
 const style_even = preload("res://styles/ResultRatingStyleEven.tres")
@@ -27,9 +27,24 @@ func _ready():
 func set_percentage(val):
 	percentage = val
 	percentage_label.text = " (%.2f %%)" % (val*100.0)
+	queue_redraw()
 func set_rating(val):
 	rating = val
-	rating_label.add_theme_color_override("font_color", HBJudge.RATING_TO_COLOR[val])
-	rating_label.add_theme_color_override("font_outline_modulate", HBJudge.RATING_TO_COLOR[val])
-	rating_label.text = HBJudge.RATING_TO_TEXT_MAP[val]
-	
+	var k := HBJudge.JUDGE_RATINGS.find_key(val)
+	rating_rect.texture = ResourcePackLoader.get_graphic(k.to_lower() + ".png")
+
+const RATING_TO_COLOR = {
+	HBJudge.JUDGE_RATINGS.COOL: Color("#ffd022"),
+	HBJudge.JUDGE_RATINGS.FINE: Color("#4ebeff"),
+	HBJudge.JUDGE_RATINGS.SAFE: Color("#00a13c"),
+	HBJudge.JUDGE_RATINGS.SAD: Color("#57a9ff"),
+	HBJudge.JUDGE_RATINGS.WORST: Color("#e470ff")
+}
+
+func get_rating_color() -> Color:
+	return RATING_TO_COLOR[rating]
+
+func _draw() -> void:
+	var col := get_rating_color()
+	col.a = 0.5
+	draw_rect(Rect2(Vector2.ZERO, size * Vector2(percentage, 1.0)), col, true)
--- rythm_game/results_screen/ResultsScreenScoreInfoDisplay.gd
+++ rythm_game/results_screen/ResultsScreenScoreInfoDisplay.gd
@@ -3,14 +3,18 @@ extends MarginContainer
 class_name HBResultsScreenScoreInfoDisplay
 
 @onready var rating_label: Label = get_node("%RatingLabel")
+@onready var rating_badge_texture_rect: TextureRect = get_node("%RatingTextureRect")
 
 @onready var percentage_new_record_label: RichTextLabel = get_node("%PercentageNewRecordLabel")
 @onready var new_percentage_label: Label = get_node("%NewPercentageLabel")
-@onready var percentage_delta_label: Label = get_node("%PercentageDeltaLabel")
+@onready var percentage_delta_label: RichTextLabel = get_node("%PercentageDeltaLabel")
 
 @onready var score_new_record_label: RichTextLabel = get_node("%ScoreNewRecordLabel")
 @onready var new_score_label: Label = get_node("%NewScoreLabel")
-@onready var score_delta_label: Label = get_node("%ScoreDeltaLabel")
+@onready var score_delta_label: RichTextLabel = get_node("%ScoreDeltaLabel")
+
+const DELTA_NEGATIVE_COLOR = Color("#57a9ff")
+const DELTA_POSITIVE_COLOR = Color("#57ff59")
 
 var result_update_queued := false
 
@@ -32,7 +36,9 @@ func _update_result():
 	if not result:
 		rating_label.text = "..."
 	else:
-		rating_label.text = HBUtils.find_key(HBResult.RESULT_RATING, result.get_result_rating())
+		var result_rating := result.get_result_rating()
+		rating_label.text = HBUtils.find_key(HBResult.RESULT_RATING, result_rating)
+		rating_badge_texture_rect.texture = HBUtils.get_clear_badge(result_rating)
 	_update_percentage()
 	_update_score()
 	
@@ -51,7 +57,19 @@ func _update_score():
 		score_delta_label.show()
 		var sign := "-" if sign(new_score - prev_score) == -1 else "+"
 		score_delta_label.text = sign + HBUtils.thousands_sep(abs(new_score-prev_score))
-	
+		score_delta_label.clear()
+		score_delta_label.push_paragraph(HORIZONTAL_ALIGNMENT_CENTER)
+		var new_text := tr("Best: {score_best}", &"Used in the results screen info display")
+		new_text = new_text.format({
+			"score_best": HBUtils.thousands_sep(max(new_score, prev_score)) + " ("
+		})
+		score_delta_label.add_text(new_text)
+		var color := DELTA_NEGATIVE_COLOR if sign == "-" else DELTA_POSITIVE_COLOR
+		score_delta_label.push_color(color)
+		score_delta_label.add_text(sign + HBUtils.thousands_sep(abs(new_score-prev_score)))
+		score_delta_label.pop()
+		score_delta_label.add_text(")")
+		score_delta_label.pop()
 func _update_percentage():
 	percentage_delta_label.hide()
 	percentage_new_record_label.hide()
@@ -68,4 +86,16 @@ func _update_percentage():
 		percentage_delta_label.show()
 		percentage_new_record_label.visible = prev_percentage < new_percentage
 		var sign := "-" if sign(new_percentage - prev_percentage) == -1 else "+"
-		percentage_delta_label.text = sign + "%.2f%%" % [abs(new_percentage-prev_percentage) * 100.0]
+		var color := DELTA_NEGATIVE_COLOR if sign == "-" else DELTA_POSITIVE_COLOR
+		percentage_delta_label.clear()
+		percentage_delta_label.push_paragraph(HORIZONTAL_ALIGNMENT_CENTER)
+		var new_text := tr("Best: {percentage_best}", &"Used in the results screen info display")
+		new_text = new_text.format({
+			"percentage_best": "%.2f%%" % [max(new_percentage, prev_percentage) * 100.0] + " ("
+		})
+		percentage_delta_label.add_text(new_text)
+		percentage_delta_label.push_color(color)
+		percentage_delta_label.add_text(sign + "%.2f%%" % [abs(new_percentage-prev_percentage) * 100.0])
+		percentage_delta_label.pop()
+		percentage_delta_label.add_text(")")
+		percentage_delta_label.pop()
