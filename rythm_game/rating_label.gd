c4436cd2092525c0803defc01f6982a83e0aa68c
Álex Román <eirexe@eirteam.moe>
2025-01-04 23:07:44 +0100

    Make new combo numbers use slight coloring

--- rythm_game/rating_label.gd
+++ rythm_game/rating_label.gd
@@ -17,10 +17,11 @@ var tween: Tween
 var current_judgement_tex: AtlasTexture
 var current_combo: int
 var judgement_wrong: bool
+var combo_color: Color
 
-const JUDGEMENT_HEIGHT = 100
+const JUDGEMENT_HEIGHT = 80
 const WRONG_CROSS_HEIGHT := 48
-const JUDGEMENT_TO_COMBO_MARGIN := 10
+const JUDGEMENT_TO_COMBO_MARGIN := 7
 
 func _ready() -> void:
 	texture_filter = TextureFilter.TEXTURE_FILTER_LINEAR_WITH_MIPMAPS_ANISOTROPIC
@@ -47,6 +48,14 @@ func get_real_texture_rect(texture: AtlasTexture, draw_rect: Rect2) -> Rect2:
 	var tex_scale := (draw_rect.size.y / tex_size.y)
 	var t := draw_rect.position + texture.margin.position * tex_scale
 	return Rect2(t, texture.region.size * tex_scale)
+
+const RATING_TO_COLOR = {
+	HBJudge.JUDGE_RATINGS.COOL: Color("#ffd022"),
+	HBJudge.JUDGE_RATINGS.FINE: Color("#4ebeff"),
+	HBJudge.JUDGE_RATINGS.SAFE: Color("#00a13c"),
+	HBJudge.JUDGE_RATINGS.SAD: Color("#57a9ff"),
+	HBJudge.JUDGE_RATINGS.WORST: Color("#e470ff")
+}
 
 func _draw():
 	if current_judgement_tex:
@@ -76,13 +85,13 @@ func _draw():
 			for c in combo_str:
 				var combo_number_texture := COMBO_NUMBER_TEXTURES[int(c)]
 				var combo_tex_size := fit_texture_in_height(combo_number_texture, JUDGEMENT_HEIGHT)
-				combo_pos.x -= 40
+				combo_pos.x -= 35
 				var combo_texture_rect := Rect2(combo_pos, combo_tex_size)
 				var real_combo_texture_rect := get_real_texture_rect(combo_number_texture, combo_texture_rect)
 				combo_pos.x = real_combo_texture_rect.position.x + real_combo_texture_rect.size.x
 				#draw_rect(combo_texture_rect, Color.BLUE, false)
 				#draw_rect(real_combo_texture_rect, Color.BLUE, false)
-				combo_number_texture.draw_rect(get_canvas_item(), combo_texture_rect, false)
+				combo_number_texture.draw_rect(get_canvas_item(), combo_texture_rect, false, combo_color.lightened(0.7))
 		
 		if judgement_wrong:
 			var wrong_rating_position := center
@@ -96,6 +105,7 @@ func _draw():
 		#draw_rect(Rect2(judgement_tex_pos, judgement_size), Color.BLUE, false)
 		#draw_rect(Rect2(judgement_pos_real-judgement_size_real*0.5, judgement_size_real), Color.GREEN, false)
 		#draw_circle(wrong_rating_position, 5.0, Color.REBECCA_PURPLE)
+
 func show_judgement(new_position: Vector2, judgement: HBJudge.JUDGE_RATINGS, wrong: bool, combo: int):
 	if tween:
 		tween.kill()
@@ -112,11 +122,12 @@ func show_judgement(new_position: Vector2, judgement: HBJudge.JUDGE_RATINGS, wro
 	size = Vector2.ZERO
 	queue_redraw()
 	if (position.y - JUDGEMENT_HEIGHT * 0.5) > ResourcePackLoader.current_skin.rating_label_top_margin:
-		position.y -= 64
+		position.y -= JUDGEMENT_HEIGHT
 	else:
-		position.y += 64
+		position.y += JUDGEMENT_HEIGHT
 	judgement_wrong = wrong
 	current_combo = combo
+	combo_color = RATING_TO_COLOR[judgement]
 	#current_judgement_tex = JUDGEMENT_TEXTURES[judgement]
 	#if worst:
 		#var center := size * 0.5
